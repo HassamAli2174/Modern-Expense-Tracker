c9bad084391eee082cb4f4683f52edbb0d491bd7
HassamAli2174 <hassamarshad021@gmail.com>
2025-05-17 21:32:29 +0500

    Refactor NewExpense widget to accept onAddExpense callback; update Expenses widget to handle expense addition and enable scrollable modal bottom sheet.

--- lib/widgets/expenses_list/new_expense.dart
+++ lib/widgets/expenses_list/new_expense.dart
@@ -5,7 +5,9 @@ import 'package:expense_tracker_app/model/expense.dart';
 // final formatter = DateFormat.yMd();
 
 class NewExpense extends StatefulWidget {
-  const NewExpense({super.key});
+  const NewExpense({super.key, required this.onAddExpense});
+
+  final void Function(Expense expense) onAddExpense;
 
   @override
   State<NewExpense> createState() => _NewExpenseState();
@@ -61,6 +63,15 @@ class _NewExpenseState extends State<NewExpense> {
       );
       return;
     }
+    widget.onAddExpense(
+      Expense(
+        title: _titleController.text,
+        amount: enteredAmount,
+        date: _selectedDate!,
+        category: _selectedValue,
+      ),
+    );
+    Navigator.pop(context);
   }
 
   @override
